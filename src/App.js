e989a189325638de7990c95b320a69e3eea999ab
srinikethanreddy01 <srinikethanreddy01@gmail.com>
2023-12-08 18:46:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
+import axios from 'axios'
+import Login from './Login.js';
+import Home from './Home.js';
+import SignUp from './SignUp.js';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import  {Toaster} from 'react-hot-toast'
+axios.defaults.baseURL='http://localhost:8000'
+axios.defaults.withCredentials=true
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Router>
+      <Toaster position='bottom-right' toastOptions={{duration:2000}}></Toaster>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route exact path="/signup" element={<SignUp />}></Route>
+          <Route exact path="/home" element={<Home />}></Route>
+        </Routes>
+      </Router>
+      
+      
     </div>
   );
 }
